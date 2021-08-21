9738f82d336bed35e474d365e5ec437c1d6fce87
Deva Mamidi <deva.mamidi.94@gmail.com>
2021-08-21 13:24:24 +0530

    Changed RestTemplate to WebClient

--- movie-catalogue-service/src/main/java/com/deva/mamidi/moviecatalogueservice/controller/MovieCatalogueController.java
+++ movie-catalogue-service/src/main/java/com/deva/mamidi/moviecatalogueservice/controller/MovieCatalogueController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,6 +21,9 @@ import java.util.stream.Collectors;
 public class MovieCatalogueController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder builder;
 
     @GetMapping("{userId}")
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId){
@@ -33,7 +37,14 @@ public class MovieCatalogueController {
         //Step -2 map through ratings and call movie api with rest template
         return ratings.stream().map(
                 rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+ rating.getMovieId(), Movie.class);
+                    //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+ rating.getMovieId(), Movie.class);
+
+                    Movie movie = builder.build()
+                            .get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+
                     return new CatalogueItem(movie.getName(), "Test", rating.getRating());
                 }
         ).collect(Collectors.toList());
