4dfe04b8f802250a2396a817cc0e80a821f41766
Deva Mamidi <deva.mamidi.94@gmail.com>
2021-08-21 10:23:19 +0530

    Initial commit - starter projects

---
+++ movie-catalogue-service/src/test/java/com/deva/mamidi/moviecatalogueservice/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deva.mamidi.moviecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
